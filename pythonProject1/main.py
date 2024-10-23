9e8f8c021691de29a492c4f82e06abb278edddc8
304 - Analisis de Datos <304@laboratorios.usbbog.edu.co>
2024-10-22 20:52:35 -0500

    nuevo

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -6,13 +6,78 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import math
 import datetime
 import platform
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 import os
+from sklearn.manifold import TSNE
+from sklearn.model_selection import train_test_split
+
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 # Load dataset
-train = pd.read_csv('/kaggle/input/digit-recognizer/train.csv')
+train = pd.read_csv('/kaggle/input//train.csv')
 test = pd.read_csv('/kaggle/input/digit-recognizer/test.csv')
+
+
+
+print('train:', train.shape)
+print('test:', test.shape)
+X = train.iloc[:, 1:785]
+y = train.iloc[:, 0]
+X_test = test.iloc[:, 0:784]
+tsne = TSNE()
+
+print('train:', train.shape)
+print('test:', test.shape)
+X = train.iloc[:, 1:785]
+y = train.iloc[:, 0]
+X_test = test.iloc[:, 0:784]
+
+""" 
+X_tsn = X/255
+tsne_res = tsne.fit_transform(X_tsn)
+plt.figure(figsize=(14, 12))
+plt.scatter(tsne_res[:,0], tsne_res[:,1], c=y, s=2)
+plt.xticks([])
+plt.yticks([])
+plt.colorbar()
+plt.show()
+"""
+
+X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size = 0.2,random_state = 1212)
+""" print('X_train:', X_train.shape)
+print('y_train:', y_train.shape)
+print('X_validation:', X_validation.shape)
+print('y_validation:', y_validation.shape)
+"""
+x_train_re = X_train.to_numpy().reshape(33600, 28, 28)
+y_train_re = y_train.values
+x_validation_re = X_validation.to_numpy().reshape(8400, 28, 28)
+y_validation_re = y_validation.values
+x_test_re = test.to_numpy().reshape(28000, 28, 28)
+
+print('x_train:', x_train_re.shape)
+print('y_train:', y_train_re.shape)
+print('x_validation:', x_validation_re.shape)
+print('y_validation:', y_validation_re.shape)
+print('x_test:', x_test_re.shape)
+
+# Save image parameters to the constants that we will use later for data re-shaping and for model traning.
+(_, IMAGE_WIDTH, IMAGE_HEIGHT) = x_train_re.shape
+IMAGE_CHANNELS = 1
+print('IMAGE_WIDTH:', IMAGE_WIDTH);
+print('IMAGE_HEIGHT:', IMAGE_HEIGHT);
+print('IMAGE_CHANNELS:', IMAGE_CHANNELS);
+
+pd.DataFrame(x_train_re[0])
+
+numbers_to_display = 25
+num_cells = math.ceil(math.sqrt(numbers_to_display))
+plt.figure(figsize=(10,10))
+for i in range(numbers_to_display):
+    plt.subplot(num_cells, num_cells, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train_re[i], cmap=plt.cm.binary)
+    plt.xlabel(y_train_re[i])
+plt.show()
